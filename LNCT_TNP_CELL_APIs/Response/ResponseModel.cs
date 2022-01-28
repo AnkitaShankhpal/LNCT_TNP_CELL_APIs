8443c502aa553dc8159661bb03002fc0b2e1bb7f
Ankita shankhpal <ankitashankhpal23@gmail.com>
2022-01-28 18:56:17 +0530

    Final Commit

---
+++ LNCT_TNP_CELL_APIs/Response/ResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoApiUsingJWT.Response
+{
+    public class ResponseModel
+    {
+        public string Message { get; set;}
+
+        public bool Status { get; set;}
+
+        public object Result { get; set; }
+    }
+}
