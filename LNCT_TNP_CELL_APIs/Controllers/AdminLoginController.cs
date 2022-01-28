3bb16112997181066a659de5eac32498c0502416
Ankita shankhpal <ankitashankhpal23@gmail.com>
2022-01-28 18:47:17 +0530

    Final Commit

---
+++ LNCT_TNP_CELL_APIs/Controllers/AdminLoginController.cs
@@ -0,0 +1,26 @@
+﻿using DemoApiUsingJWT.Service;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoApiUsingJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminLoginController : ControllerBase
+    {
+        private ILoginService _loginService;
+        public AdminLoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost]
+        public IActionResult Admin_Login(string UserName, string password)
+        {
+            var str = _loginService.Admin_Login(UserName, password);    
+            return Ok(str);
+        }
+
+    }
+}
